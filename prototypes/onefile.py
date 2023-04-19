fbae4982935e8fd5f0d07c7f2b85c19c21d51423
Rohan Fletcher <rohfle@gmail.com>
2023-04-19 22:32:00 +1200

    Tidyup

--- prototypes/onefile.py
+++ prototypes/onefile.py
@@ -5,55 +5,23 @@ import numpy as np
 from flax import linen as nn
 from tqdm import tqdm
 import time
-RUN_TIMESTAMP = int(time.time())
+from clu import metrics
+from flax.training import train_state  # Useful dataclass to keep train state
+from flax import struct                # Flax dataclasses
+import optax                           # Common loss functions and optimizers
+
 
 np.set_printoptions(precision=2, linewidth=140)
 
-import time
-# a decorator to time functions
-def timeit(func):
-    def runit(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        duration = time.time() - start_time
-        print("time ", func.__name__, ":", duration)
-        return result
-    return runit
 
-def timediter(iterable, label):
-    it = iter(iterable)
-    while True:
-        try:
-            start = time.time()
-            value = next(it)
-            end = time.time()
-            print(f"time {label} yield: {end - start:.6f} seconds")
-            yield value
-        except StopIteration:
-            break
+RUN_TIMESTAMP = int(time.time())
+BATCH_SIZE = 32 # TODO: change
+NUM_EPOCHS = 1 # TODO: change to 100 or something
+IMAGE_DTYPE = jnp.bfloat16
+SHUFFLE_SEED = 42
+SHUFFLE_BUFFER_SIZE = 10_000
+USE_STREAMING = True
 
-def profile(name, limit=3):
-    def outer(func):
-        left = limit
-        def runit(*args, **kwargs):
-            if left > 0:
-                with jax.profiler.trace(name, create_perfetto_link=True):
-                    return func(*args, **kwargs)
-            else:
-                return func(*args, **kwargs)
-        return runit
-    return outer
-
-def profileiter(iterable, label):
-    it = iter(iterable)
-    try:
-        with jax.profiler.trace(label, create_perfetto_link=True):
-            yield next(it)
-
-        while True:
-            yield next(it)
-    except StopIteration:
-        pass
 
 
 
@@ -75,14 +43,7 @@ class CNN(nn.Module):
         return x
 
 
-BATCH_SIZE = 32 # TODO: change
-NUM_EPOCHS = 1 # TODO: change to 100 or something
-IMAGE_DTYPE = jnp.bfloat16
 
-SHUFFLE_SEED = 42
-SHUFFLE_BUFFER_SIZE = 10_000
-
-USE_STREAMING = True
 if USE_STREAMING:
     dataset = load_dataset('fashion_mnist', streaming=True).shuffle(seed=SHUFFLE_SEED, buffer_size=SHUFFLE_BUFFER_SIZE)
 else:
@@ -112,10 +73,6 @@ ds_train = dataset['train'].map(transform_and_collate, batched=True, batch_size=
 ds_valid = dataset['test'].map(transform_and_collate, batched=True, batch_size=BATCH_SIZE, drop_last_batch=True)
 image_shape = np.array(next(iter(ds_train))['image']).shape[1:]
 
-from clu import metrics
-from flax.training import train_state  # Useful dataclass to keep train state
-from flax import struct                # Flax dataclasses
-import optax                           # Common loss functions and optimizers
 
 @struct.dataclass
 class Metrics(metrics.Collection):
