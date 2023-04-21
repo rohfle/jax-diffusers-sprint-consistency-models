32a1528427fa2c23d1d587279398b879c2efc3bb
Rohan Fletcher <rohfle@gmail.com>
2023-04-21 16:34:55 +1200

    Removed unused state fields

--- src/state.py
+++ src/state.py
@@ -1,15 +1,8 @@
-from clu import metrics
 from flax.training import train_state  # Useful dataclass to keep train state
 from flax import struct                # Flax dataclasses
 from flax import core
-from typing import Any
-
-@struct.dataclass
-class Metrics(metrics.Collection):
-    accuracy: metrics.Accuracy
-    loss: metrics.Average.from_output('loss')
+from typing import Any, Callable
 
 class TrainState(train_state.TrainState):
-    loss_fn: Any
+    loss_fn: Callable = struct.field(pytree_node=False)
     ema_params: core.FrozenDict[str, Any] = struct.field(pytree_node=True)
-    metrics: Metrics
